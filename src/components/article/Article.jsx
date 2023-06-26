eb621bb2019bab2cab556d8c7dd43150e63e7810
Islam Kamal <islamkamalx1>
2023-06-26 23:41:02 +0300

    Blog & Article

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,19 @@
-import "./article.css"
-const Article = () => {
+import "./article.css";
+// eslint-disable-next-line react/prop-types
+const Article = ({ imgUrl, date, title }) => {
   return (
-    <div>Article</div>
-  )
-}
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog" />
+      </div>
 
-export default Article
+      <div className="gpt3__blog-container_article-content">
+        <p>{date}</p>
+        <h3>{title}</h3>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
